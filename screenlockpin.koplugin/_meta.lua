d8b64d7261abd08d196cedf528c9627de28d4ea2
Ole Asteo <oleasteo@mailup.net>
2025-11-16 12:32:10 +0800

    build: release v2025.11-1

--- screenlockpin.koplugin/_meta.lua
+++ screenlockpin.koplugin/_meta.lua
@@ -7,7 +7,7 @@ return {
     description = _([[Protect your device privacy with a PIN.]]),
 
     -- additional meta information; no effect in code
-    version = "2025.11",
+    version = "2025.11-1",
     website_url = "https://github.com/oleasteo/koreader-screenlockpin",
     repository_url = "https://github.com/oleasteo/koreader-screenlockpin.git",
 }
